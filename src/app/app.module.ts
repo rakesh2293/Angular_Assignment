a7239eca347c3cccb7e0437d72d5a875614bfe2e
rakeshr <rakesh.r@flutura.com>
2021-04-25 16:00:55 +0530

    code push to main branch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
-
+import { CustomerDetailsComponent } from './components/customer-details/customer-details.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HighchartsChartModule } from 'highcharts-angular';
+import {MatIconModule} from '@angular/material/icon';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomerDetailsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    HighchartsChartModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
